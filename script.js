dcfca17c01862c27d3f0052728e82a0e07d91084
Hizude <k.serkanertas@gmail.com>
2020-08-08 16:50:58 +0200

    finished game

--- script.js
+++ script.js
@@ -1,19 +1,48 @@
-const holes = document.querySelectorAll(".hole");
-const scoreBoard = document.querySelector(".score");
-const moles = document.querySelectorAll(".mole");
+const holes = document.querySelectorAll('.hole');
+const scoreBoard = document.querySelector('.score');
+const moles = document.querySelectorAll('.mole');
 let lastHole;
+let timeUp = false;
+let score = 0;
 
 function randTime(min, max) {
-  return Math.round(Math.random() * (max - min) + min);
+    return Math.round(Math.random() * (max - min) + min);
 }
 
 function randHole(holes) {
-  const index = Math.floor(Math.random() * holes.length);
-  const hole = holes[index];
-  if (hole === lastHole) {
-    return randHole(holes);
-  }
+    const index = Math.floor(Math.random() * holes.length);
+    const hole = holes[index];
+    if (hole === lastHole) {
+        return randHole(holes);
+    }
 
-  lastHole = hole;
-  return hole;
+    lastHole = hole;
+    return hole;
 }
+
+function peek() {
+    const time = randTime(200, 1000);
+    const hole = randHole(holes);
+    hole.classList.add('up');
+    setTimeout(() => {
+        hole.classList.remove('up');
+        if (!timeUp) peek();
+    }, time);
+}
+
+function startGame() {
+    scoreBoard.textContent = 0;
+    timeUp = false;
+    score = 0;
+    peek();
+    setTimeout(() => (timeUp = true), 10000);
+}
+
+function bonk(event) {
+    if (!event.isTrusted) return;
+    score++;
+    this.classList.remove('up');
+    scoreBoard.textContent = score;
+}
+
+moles.forEach((mole) => mole.addEventListener('click', bonk));
